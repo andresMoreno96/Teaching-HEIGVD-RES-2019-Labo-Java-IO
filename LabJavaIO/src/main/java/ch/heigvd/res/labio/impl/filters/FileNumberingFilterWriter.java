9ba8136e9a6eda06a0904e064d940a53ba5079c8
andy <andres.moreno@heig-vd.ch>
2019-03-16 19:07:43 +0100

    FileNumberingFilterWriter now works

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -10,32 +10,67 @@ import java.util.logging.Logger;
  * When filter encounters a line separator, it sends it to the decorated writer.
  * It then sends the line number and a tab character, before resuming the write
  * process.
- *
+ * <p>
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private int lineNumber = 0;
+    private char recent;
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+    public FileNumberingFilterWriter(Writer out) {
+        super(out);
+    }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        for (int i = off; i < off + len; ++i) {
+            write((int) str.charAt(i));
+        }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        for (int i = off; i < off + len; ++i) {
+            write((int) cbuf[i]);
+        }
+
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+
+        if (lineNumber==0) {
+            out.write(++lineNumber + "\t");
+            out.write(c);
+
+        } else {
+
+            if ((char) c == '\n') {
+                out.write(c);
+                out.write(++lineNumber + "\t");
+
+
+
+                // Detects \r
+            } else if (recent == '\r') {
+                out.write(++lineNumber + "\t");
+                out.write(c);
+
+            } else {
+                out.write(c);
+            }
+        }
+
+        recent = (char) c;
+    }
+
+
+
 
 }
